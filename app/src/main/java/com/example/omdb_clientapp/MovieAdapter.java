99a5481b9a27ba6cfdbd279c63f947206a31dca3
ankit <ankitv397@gmail.com>
2020-05-21 20:56:13 +0530

    All data on adpater is placed successfully

--- app/src/main/java/com/example/omdb_clientapp/MovieAdapter.java
+++ app/src/main/java/com/example/omdb_clientapp/MovieAdapter.java
@@ -3,12 +3,14 @@ package com.example.omdb_clientapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.omdb_clientapp.model.SearchResult;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -31,9 +33,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         SearchResult result = results.get(position);
         holder.titleview.setText(result.getTitle());
-        holder.imdbview.setText(result.getImdbID());
         holder.typeview.setText(result.getType());
         holder.yearview.setText(result.getYear());
+        Picasso.get().load(result.getPoster()).into(holder.imageview);
     }
 
     @Override
@@ -45,15 +47,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
         TextView titleview;
         TextView yearview;
-        TextView imdbview;
         TextView typeview;
-
+        ImageView imageview;
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             titleview = itemView.findViewById(R.id.item_search_results_title_view);
             yearview = itemView.findViewById(R.id.item_search_results_year_view);
             typeview = itemView.findViewById(R.id.item_search_results_type_view);
-            imdbview = itemView.findViewById(R.id.item_search_results_rating_view);
+            imageview = itemView.findViewById(R.id.item_search_results_image_view);
         }
     }
 
