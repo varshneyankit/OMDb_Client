411ccaf4f146c124be81de6b17d75728e97f582d
singaltanmay <tanmaysingal@icloud.com>
2020-05-29 02:42:06 +0530

    Ratings placement + failed attempt genre horiz texts

--- app/src/main/java/com/example/omdb_clientapp/ui/MovieDataFragment.java
+++ app/src/main/java/com/example/omdb_clientapp/ui/MovieDataFragment.java
@@ -5,12 +5,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.omdb_clientapp.R;
 import com.example.omdb_clientapp.api.RetrofitClient;
@@ -67,17 +71,17 @@ public class MovieDataFragment extends Fragment {
     }
 
     private void setMetaData(ResultMetadata body) {
-        ImageView poster = getView().findViewById(R.id.fragment_movie_data_poster);
-        TextView title = getView().findViewById(R.id.fragment_movie_data_title);
-        TextView year = getView().findViewById(R.id.fragment_movie_data_year);
-        TextView rated = getView().findViewById(R.id.fragment_movie_data_rated);
-        // TODO implement genre buttons
-        TextView genre = getView().findViewById(R.id.fragment_movie_data_directors);
-        TextView runtime = getView().findViewById(R.id.fragment_movie_data_runtime);
-        TextView imdbRatings = getView().findViewById(R.id.fragment_movie_data_imdbRating);
-        TextView plot = getView().findViewById(R.id.fragment_movie_data_plot);
-        TextView directors = getView().findViewById(R.id.fragment_movie_data_directors);
-        TextView cast = getView().findViewById(R.id.fragment_movie_data_cast);
+        View parentView = getView();
+
+        ImageView poster = parentView.findViewById(R.id.fragment_movie_data_poster);
+        TextView title = parentView.findViewById(R.id.fragment_movie_data_title);
+        TextView year = parentView.findViewById(R.id.fragment_movie_data_year);
+        TextView rated = parentView.findViewById(R.id.fragment_movie_data_rated);
+        TextView runtime = parentView.findViewById(R.id.fragment_movie_data_runtime);
+        TextView imdbRatings = parentView.findViewById(R.id.fragment_movie_data_imdbRating);
+        TextView plot = parentView.findViewById(R.id.fragment_movie_data_plot);
+        TextView directors = parentView.findViewById(R.id.fragment_movie_data_directors);
+        TextView cast = parentView.findViewById(R.id.fragment_movie_data_cast);
 
         Picasso.get().load(body.getPoster()).into(poster);
         title.setText(body.getTitle());
@@ -85,10 +89,40 @@ public class MovieDataFragment extends Fragment {
         rated.setText(body.getRated());
         directors.setText(body.getDirector());
 
+        directors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        // TODO implement genre buttons
+//        HorizontalScrollView scrollView = parentView.findViewById(R.id.fragment_movie_data_genre_hscroll_view);
         String[] genres = body.getGenre().split(", ");
-        if (genres.length > 0) {
-            genre.setText(genres[0]);
-        }
+
+//        for (String genre : genres) {
+//            View inflate = getLayoutInflater().inflate(R.layout.item_genre_movie_data, null);
+//            Button textButton = inflate.findViewById(R.id.movie_genre_text);
+//            textButton.setText(genre);
+//            scrollView.addView(inflate);
+//        }
+
+//        GridView gridView = parentView.findViewById(R.id.fragment_movie_data_genre_grid_view);
+//        gridView.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
+//
+//        gridView.setAdapter(new ArrayAdapter<String>(this.getContext(), R.layout.item_genre_movie_data, genres) {
+//            @NonNull
+//            @Override
+//            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//                if (convertView == null) {
+//                    convertView = getLayoutInflater().inflate(R.layout.item_genre_movie_data, parent, false);
+//                }
+//                Button textButton = convertView.findViewById(R.id.movie_genre_text);
+//                textButton.setText(genres[position]);
+//                return convertView;
+//            }
+//        });
 
         runtime.setText(body.getRuntime());
         imdbRatings.setText(body.getImdbRating() + " / 10");
